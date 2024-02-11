a7a303b66d56ea3c3afd5962e1e8bd5df5eb3ceb
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-02-11 15:21:11 -0600

    saving dog router

--- src/server/api/routers/dog.ts
+++ src/server/api/routers/dog.ts
@@ -7,24 +7,98 @@ import {
 } from "~/server/api/trpc";
 
 export const dogRouter = createTRPCRouter({
-  createMany: protectedProcedure
+  // createMany: protectedProcedure
+  //   .input(
+  //     z
+  //       .object({
+  //         id: z.number(),
+  //         name: z.string().min(1),
+  //         age: z.string().min(1),
+  //         breed: z.string().min(1),
+  //         gender: z.string().min(1),
+  //         photos: z
+  //           .object({
+  //             id: z.number(),
+  //             full: z.string().nullable(),
+  //             large: z.string().nullable(),
+  //             medium: z.string().nullable(),
+  //             small: z.string().nullable(),
+  //             dogId: z.number(),
+  //           })
+  //           .array(),
+  //       })
+  //       .array(),
+  //   )
+  //   .mutation(async ({ ctx, input }) => {
+  //     try {
+  //       console.log("input", input);
+  //       const dogs = await ctx.db.dog.createMany({
+  //         data: input,
+  //       });
+  //       console.log("dogs", dogs);
+  //       return dogs;
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   }),
+  createOne: protectedProcedure
     .input(
-      z
-        .object({
-          id: z.number(),
-          name: z.string().min(1),
-          age: z.string().min(1),
-          breed: z.string().min(1),
-          gender: z.string().min(1),
-          photo: z.string().nullable(),
-        })
-        .array(),
+      z.object({
+        id: z.number(),
+        name: z.string().min(1),
+        age: z.string().min(1),
+        breed: z.string().min(1),
+        gender: z.string().min(1),
+        photos: z
+          .object({
+            id: z.string().cuid(),
+            full: z.string(),
+            large: z.string(),
+            medium: z.string(),
+            small: z.string(),
+            dogId: z.number(),
+          })
+          .array(),
+        address: z.object({
+          // id: z.string().cuid(),
+          address1: z.string().nullable(),
+          address2: z.string().nullable(),
+          city: z.string().nullable(),
+          zipCode: z.string().nullable(),
+          state: z.string().nullable(),
+          dogId: z.number(),
+        }),
+      }),
     )
     .mutation(async ({ ctx, input }) => {
       try {
-        console.log("input", input);
-        const dogs = await ctx.db.dog.createMany({
-          data: input,
+        // console.log("input", input);
+        const dogs = await ctx.db.dog.create({
+          data: {
+            id: input.id,
+            name: input.name,
+            age: input.age,
+            breed: input.breed,
+            gender: input.gender,
+            photos: {
+              create: input.photos.map((photo) => ({
+                full: photo.full,
+                large: photo.large,
+                medium: photo.medium,
+                small: photo.small,
+              })),
+            },
+            address: {
+              create: {
+                address1: input.address.address1,
+                address2: input.address.address2,
+                city: input.address.city,
+                zipCode: input.address.zipCode,
+                state: input.address.state,
+                // dogId: input.dogId
+              },
+            },
+          },
         });
         console.log("dogs", dogs);
         return dogs;
