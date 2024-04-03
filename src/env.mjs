bc4a02f420b6124cddcebc817bbfa731f50c2744
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2024-04-03 10:26:51 -0400

    updated env.mjs to include updated variables

--- src/env.mjs
+++ src/env.mjs
@@ -30,7 +30,15 @@ export const env = createEnv({
     ),
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
-    // POSTGRES_URL: z.string(),
+    POSTGRES_URL: z.string().url(),
+    POSTGRES_PRISMA_URL: z.string().url(),
+    POSTGRES_URL_NO_SSL: z.string().url(),
+    POSTGRES_URL_NON_POOLING: z.string().url(),
+    POSTGRES_USER: z.string(),
+    POSTGRES_HOST: z.string(), 
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DATABASE: z.string()
+
   },
 
   /**
@@ -56,8 +64,15 @@ export const env = createEnv({
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-    NEXT_PUBLIC_API_SECRET: process.env.NEXT_PUBLIC_API_SECRET
-    
+    NEXT_PUBLIC_API_SECRET: process.env.NEXT_PUBLIC_API_SECRET,
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
